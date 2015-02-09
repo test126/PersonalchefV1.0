9193840951af469d01149ab76d7ed30661d3504e
test126 <testaccount@126.com>
2015-02-09 17:51:21 +0800

    111

--- PersonalChef/src/com/goodfriends/personalchef/util/VolleyImage.java
+++ PersonalChef/src/com/goodfriends/personalchef/util/VolleyImage.java
@@ -7,18 +7,27 @@ import com.android.volley.toolbox.ImageLoader.ImageListener;
 import com.goodfriends.personalchef.R;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class VolleyImage {
 
 	private Context context;
 	private BitmapCache imageCache;
-	RequestQueue mRequestQueue;
-	ImageLoader mImageLoader;
+	private RequestQueue mRequestQueue;
+	private ImageLoader mImageLoader;
+	private int width, height;
 	int defaultPic = R.drawable.nopic;
 	public VolleyImage(Context context) {
 		super();
 		this.context = context;
+		WindowManager manager = (WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE);
+		DisplayMetrics dm = new DisplayMetrics();
+		manager.getDefaultDisplay().getMetrics(dm);
+		width = dm.widthPixels;
+		height = dm.heightPixels;
 		mRequestQueue = Volley.newRequestQueue(context); 
 		imageCache = new BitmapCache();
 		mImageLoader = new ImageLoader(mRequestQueue, imageCache);
@@ -27,7 +36,7 @@ public class VolleyImage {
 	public void loadImg(ImageView imgView,String url){
 		ImageListener listener = ImageLoader
 				.getImageListener(imgView, defaultPic,defaultPic);
-		mImageLoader.get(url, listener);
+		mImageLoader.get(url, listener,width,height);
 	}
 
 }
